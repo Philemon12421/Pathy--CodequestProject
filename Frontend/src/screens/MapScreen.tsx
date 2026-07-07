74b9123eaa1135569f515c9987ff4b34256a7846
Philemon12421 <philemonkusi292@gmail.com>
2026-07-07 02:34:24 +0000

    fixed aiscreen

--- Frontend/src/screens/MapScreen.tsx
+++ Frontend/src/screens/MapScreen.tsx
@@ -303,7 +303,11 @@ export default function MapScreen({ navigation, route }: any) {
         showsTraffic
       >
         {/* Incident markers */}
+<<<<<<< HEAD
         {incidents.map((inc: any) => (
+=======
+        {(incidents || []).map((inc: any) => (
+>>>>>>> 56a8be1 (fixed aiscreen)
           <Marker
             key={inc.id}
             coordinate={{ latitude: parseFloat(inc.latitude), longitude: parseFloat(inc.longitude) }}
@@ -322,7 +326,7 @@ export default function MapScreen({ navigation, route }: any) {
         ))}
 
         {/* Ad markers */}
-        {ads.map((ad: any) => (
+        {(ads || []).map((ad: any) => (
           <React.Fragment key={ad.id}>
             <Circle
               center={{ latitude: parseFloat(ad.latitude), longitude: parseFloat(ad.longitude) }}
@@ -523,7 +527,11 @@ export default function MapScreen({ navigation, route }: any) {
                           onPress: async () => {
                             try {
                               await incidentsAPI.delete(selectedMarker.data.id);
+<<<<<<< HEAD
                               setIncidents(incidents.filter((i: any) => i.id !== selectedMarker.data.id));
+=======
+                              setIncidents((incidents || []).filter((i: any) => i.id !== selectedMarker.data.id));
+>>>>>>> 56a8be1 (fixed aiscreen)
                               setSelectedMarker(null);
                               Alert.alert('Deleted', 'Incident report has been deleted.');
                             } catch (err: any) {
